a8005083d61b33e6f4ed1fba867af8f8698478f8
roma9196 <72673714+roma9196@users.noreply.github.com>
2020-10-17 11:38:05 -0500

    Update Connection.php

--- database/Connection.php
+++ database/Connection.php
@@ -6,7 +6,7 @@ class connection{
 	protected $transaction;
 
 	//connect to DB use the default constructor
-	public function __construct($username="sql10369981", $password="YmhqWPwT73", $host="sql10.freesqldatabase.com", $dbname="sql10369981", $options = []){
+	public function __construct($username="sql10371393", $password="cXprehqAri", $host="sql10.freesqldatabase.com", $dbname="sql10371393", $options = []){
 		
 		$this->isConn = TRUE;
 		try{
