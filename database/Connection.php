4f8ce435a71e0844ba0de99fc589fc6d30da7da1
roma9196 <72673714+roma9196@users.noreply.github.com>
2020-10-24 10:08:59 -0500

    Update Connection.php

--- database/Connection.php
+++ database/Connection.php
@@ -6,7 +6,7 @@ class connection{
 	protected $transaction;
 
 	//connect to DB use the default constructor
-	public function __construct($username="3627461_apiroma", $password="roma9196", $host="fdb30.atspace.me", $dbname="3627461_apiroma", $options = []){
+	public function __construct($username="3627461_apiroma", $password="roma9691", $host="fdb30.atspace.me", $dbname="3627461_apiroma", $options = []){
 		
 		$this->isConn = TRUE;
 		try{
